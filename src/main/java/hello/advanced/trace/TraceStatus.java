543223ef452ad93325cfc9da65a96e51b47bda10
inging12 <inging12@naver.com>
2022-07-14 11:09:16 +0900

    220714 init

---
+++ src/main/java/hello/advanced/trace/TraceStatus.java
@@ -0,0 +1,38 @@
+package hello.advanced.trace;
+
+public class TraceStatus {
+
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+
+    public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = message;
+    }
+
+    public void setTraceId(TraceId traceId) {
+        this.traceId = traceId;
+    }
+
+    public void setStartTimeMs(Long startTimeMs) {
+        this.startTimeMs = startTimeMs;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public Long getStartTimeMs() {
+        return startTimeMs;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
